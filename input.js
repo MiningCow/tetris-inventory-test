086bda7becfada9bab772bdfa649d8cc0cf7b6ad
MiningCow <40306125+MiningCow@users.noreply.github.com>
2021-10-01 17:17:49 -0400

    Add spacebar input to clear held item

--- input.js
+++ input.js
@@ -1,103 +1,99 @@
 
 window.addEventListener("keyup", event => {
     if (event.defaultPrevented) {
-      return;
-    }
-  
-    switch (event.key) {
-      case "1":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#FF0000",
-          shape: [
-            " # ",
-            "## ",
-            "#  "
-          ]
-        })
-        break;
-      case "2":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#00FF00",
-          shape: [
-            " # ",
-            " ##",
-            "  #"
-          ]
-        })
-        break;
-      case "3":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#0000FF",
-          shape: [
-            " # ",
-            " # ",
-            "## "
-          ]
-        })
-        break;
-      case "4":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#FFFF00",
-          shape: [
-            "##",
-            "##"
-          ]
-        })
-        break;
-      case "5":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#00FFFF",
-          shape: [
-            " #  ",
-            " #  ",
-            " #  ",
-            " #  "
-          ]
-        })
-        break;
-      case "6":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#FF00FF",
-          shape: [
-            " # ",
-            "###",
-            "   "
-          ]
-        })
-        break;
-      case "7":
-        heldItem = new Item({
-          x: mouseX,
-          y: mouseY,
-          origin: {x: 1, y: 1},
-          color: "#FFA500",
-          shape: [
-            " # ",
-            " # ",
-            " ##"
-          ]
-        })
-        break;
-      default:
         return;
+    }
+
+    switch (event.key) {
+        case "1":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#FF0000",
+                shape: [
+                " # ",
+                "## ",
+                "#  "
+            ]})
+            break;
+        case "2":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#00FF00",
+                shape: [
+                " # ",
+                " ##",
+                "  #"
+            ]})
+            break;
+        case "3":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#0000FF",
+                shape: [
+                " # ",
+                " # ",
+                "## "
+            ]})
+            break;
+        case "4":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#FFFF00",
+                shape: [
+                "##",
+                "##"
+            ]})
+            break;
+        case "5":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#00FFFF",
+                shape: [
+                " #  ",
+                " #  ",
+                " #  ",
+                " #  "
+            ]})
+            break;
+        case "6":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#FF00FF",
+                shape: [
+                " # ",
+                "###",
+                "   "
+            ]})
+            break;
+        case "7":
+            heldItem = new Item({
+                x: mouseX,
+                y: mouseY,
+                origin: {x: 1, y: 1},
+                color: "#FFA500",
+                shape: [
+                " # ",
+                " # ",
+                " ##"
+            ]})
+            break;
+        case " ":
+            heldItem = undefined;
+            break;
+        default:
+            return;
     }
     event.preventDefault();
 }, true);
